e3bfd7ec816abd0d6a45120a4502ba3d60827101
Katherine Bailey <katbailey@172987.no-reply.drupal.org>
2008-04-22 22:49:51 +0000

    changing jquery to conform to Drupal standards, adding effects for tabs, increasing tab limit to 12

--- js/quicktabs.js
+++ js/quicktabs.js
@@ -1,14 +1,25 @@
-$(document).ready(function(){
+Drupal.quicktabs = function() {
   $('div.quicktabs').hide();
   $('div.quicktabs:first-child').show();
   $('ul.quicktabs_tabs li:first-child').addClass('active');
-  
+  var effect = Drupal.settings.quicktabs.effect;
   var clickFunction = function() {      
     var tabIndex = this.myTabIndex;
-    $(this).parent().parent().parent().find('div.quicktabs').hide();
-    $(this).parent().siblings().removeClass('active');
-    $(this).parent().addClass('active');
-    $(this).parent().parent().parent().find('div.quicktabs:eq('+tabIndex+')').show();
+    $(this).parents('div.quicktabs_wrapper').find('div.quicktabs').hide();
+    $(this).parents('li').siblings().removeClass('active');
+    $(this).parents('li').addClass('active');
+    switch (effect) {
+      case 'none':
+        $(this).parents('div.quicktabs_wrapper').find('div.quicktabs:eq('+tabIndex+')').show();
+        break;
+      case 'fade':
+        $(this).parents('div.quicktabs_wrapper').find('div.quicktabs:eq('+tabIndex+')').fadeIn();        
+        break;
+      case 'slide':
+        $(this).parents('div.quicktabs_wrapper').find('div.quicktabs:eq('+tabIndex+')').slideDown('slow');
+        break;
+    }
+
     return false;
   };
   $('ul.quicktabs_tabs').each(function(){
@@ -18,4 +29,9 @@ $(document).ready(function(){
     });
   });
   $('ul.quicktabs_tabs li a').bind('click', clickFunction);
-});
+}
+
+
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.quicktabs);
+}
