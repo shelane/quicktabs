409729b6b5ca5eb2c90794dc5d1c96e772c67008
Katherine Bailey <katbailey@172987.no-reply.drupal.org>
2008-06-05 01:13:02 +0000

    Changes to the admin interface to allow adding and removal of tabs when creating and editing Quick Tabs blocks

--- js/quicktabs_form.js
+++ js/quicktabs_form.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
   
-  $('#quicktabs-form div.form-item :input[@name^="type"]:checked').each(function(){
+  $('#quicktabs-form div.form-item :input[@name*="tabtype"]:checked').each(function(){
     if(this.value == 'block') {
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="vid"]').parent().hide();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="args"]').parent().hide();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="limit"]').parent().hide();      
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="vid"]').parent().hide();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="args"]').parent().hide();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="limit"]').parent().hide();      
     } else {
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="bid"]').parent().hide();       
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="bid"]').parent().hide();       
     }
   });
 
   var showhide = function() {
     if (this.value == 'block') {
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="vid"]').parent().hide();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="args"]').parent().hide();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="limit"]').parent().hide();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="bid"]').parent().show();    
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="vid"]').parent().hide();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="args"]').parent().hide();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="limit"]').parent().hide();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="bid"]').parent().show();    
    } else {
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="bid"]').parent().hide();      
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="vid"]').parent().show();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="args"]').parent().show();
-      $(this).parents('fieldset').not('legend:contains("QT")')
-      .find('div.form-item :input[@name^="limit"]').parent().show();  
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="bid"]').parent().hide();      
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="vid"]').parent().show();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="args"]').parent().show();
+      $(this).parents('tr')
+      .find('div.form-item :input[@name*="limit"]').parent().show();  
    }
   };
   
-  $('#quicktabs-form div.form-item :input[@name^="type"]').bind('click', showhide);
+  $('#quicktabs-form div.form-item :input[@name*="tabtype"]').bind('click', showhide);
   
 });
